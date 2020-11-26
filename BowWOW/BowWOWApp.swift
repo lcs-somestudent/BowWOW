0ddfc3c2fa5a62fe64a6031abf0c5641574c6f2f
Russell Gordon <rgordon@lcs.on.ca>
2020-11-26 08:56:50 -0500

    Initial Commit

---
+++ BowWOW/BowWOWApp.swift
@@ -0,0 +1,17 @@
+//
+//  BowWOWApp.swift
+//  BowWOW
+//
+//  Created by Russell Gordon on 2020-11-26.
+//
+
+import SwiftUI
+
+@main
+struct BowWOWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
